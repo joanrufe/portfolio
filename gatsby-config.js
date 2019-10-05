4525372ca6151f0b7f456d4599401781e44a94b4
Tony <jferna01@tui.be>
2019-10-05 16:59:21 +0200

    Added plugin to remove fingerprints and modified schema manifest colors

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,10 @@
 const siteConfig = require('./site-config');
 const colors = require('./src/components/styled/theme');
 
+console.log('\x1b[33m%s\x1b[0m', '------ Process.env ------');
+console.log(process.env.NODE_ENV);
+console.log(process.env.NETLIFY);
+
 module.exports = {
   siteMetadata: {
     ...siteConfig
@@ -28,6 +32,7 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    `gatsby-plugin-remove-fingerprints`,
     `gatsby-plugin-styled-components`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
--- site-config.js
+++ site-config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const theme = require('./src/components/styled/theme');
 
 module.exports = {
   author: `Antonio Ruiz`,
@@ -6,8 +7,8 @@ module.exports = {
   siteTitleShort: `AR`,
   description: `Web portfolio.`,
   siteUrl: `https://antonioruiz.netlify.com`,
-  themeColor: `#ff1654`,
-  backgroundColor: `#f2ffbc`,
+  themeColor: theme.turquoise,
+  backgroundColor: theme.lightYellow,
   pathPrefix: null,
   logo: path.resolve(__dirname, 'src/images/icon-developer-100.png'),
   social: { twitter: `Anton_Devel` },
