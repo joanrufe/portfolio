c249f147ee050c6648a3ec7517b1e479d0e8e0f3
Tony <jferna01@tui.be>
2019-10-05 17:11:35 +0200

    Using gatsby-plugin-remove-fingerprints only in NETLIFY -> this should fix the cache issue when deploying

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,7 @@
 const siteConfig = require('./site-config');
 const colors = require('./src/components/styled/theme');
 
-console.log('\x1b[33m%s\x1b[0m', '------ Process.env ------');
-console.log(process.env.NODE_ENV);
-console.log(process.env.NETLIFY);
-
-module.exports = {
+const gatsbyConfig = {
   siteMetadata: {
     ...siteConfig
   },
@@ -32,10 +28,15 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
-    `gatsby-plugin-remove-fingerprints`,
     `gatsby-plugin-styled-components`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
     // 'gatsby-plugin-offline',
   ],
 }
+
+if (process.env.NETLIFY) {
+  gatsbyConfig.plugins.push('gatsby-plugin-remove-fingerprints')
+}
+
+module.exports = gatsbyConfig;
